9422de0117615f10d00bd7bd1d18d6c7959a67f2
rootbeeeer <lilianasalunat@gmail.com>
2026-02-20 15:11:54 +0800

    fixed recipelist and detail

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import recipes_list, recipe
 urlpatterns = [
-    path('', index, name='index'),
+    path('recipes_list', recipes_list, name='recipes_list'),
+    path('recipe/<int:num>', recipe, name='recipe'),
 ]
 # This might be needed, depending on your Django version
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,74 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+ctx = {
+    "recipes": [
+        {
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+            ],
+            "link": "/recipe/1"
+        },
+        {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quanity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+    ]
+}
+
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello World! This came from the index view')
+def recipes_list(request):
+    return render(request, 'recipes_list.html', ctx)
+
+def recipe(request, num=1):
+    return render(request, 'recipe.html', {"name": ctx["recipes"][num-1]["name"], "ingredients": ctx["recipes"][num-1]["ingredients"]})
