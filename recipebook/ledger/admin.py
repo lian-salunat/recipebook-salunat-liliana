ae31f5bdec008497b13079dcee1260cc4af19fb1
lian-salunat <liliana.salunat@student.ateneo.edu>
2026-05-17 18:19:00 +0800

    registered Recipe with RecipeIngredient inline admin

---
+++ recipebook/ledger/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Ingredient, Recipe, RecipeIngredient
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
